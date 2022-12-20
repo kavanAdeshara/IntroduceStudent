cd66031b28c054e93077283d573c0bfc9bfd8235
Kavan Adeshara <73504280+kavanAdeshara@users.noreply.github.com>
2022-12-19 21:52:10 -0500

    Initial Commit

---
+++ IntroduceStudent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntroduceStudent
+//
+//  Created by Kavan Adeshara on 12/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
